c0d9f65020df9776a0a4f8da12af5fb0e9ee60db
Edouard-Legoupil <edouard.legoupil@gmail.com>
2019-03-29 16:10:29 +0300

    initial commit

---
+++ pagedown.R
@@ -0,0 +1,14 @@
+## https://pagedown.rbind.io/
+## https://rlesur.gitlab.io/statcanrug/slides.pdf
+remotes::install_github('rstudio/pagedown', force = TRUE)
+install.packages("xaringan")
+
+## once xaringgan is installed - restart R studio - and use Infinite moon reader add-on to view the report
+
+
+library(pagedown)
+library(xaringan)
+
+
+
+#xaringan::inf_mr()
